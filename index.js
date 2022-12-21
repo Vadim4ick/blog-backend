489dc53d5da7f2ee54a1525ccd36546b3b4c1e18
Vadim Firulyov <firulvv@mail.ru>
2022-12-21 21:06:59 +0500

    uploads

---
+++ index.js
@@ -0,0 +1,93 @@
+import express from "express";
+import mongoose from "mongoose";
+import fs from "fs";
+import multer from "multer";
+import cors from "cors";
+
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+
+import { UserController, PostController } from "./controllers/index.js";
+import { checkAuth, handelValidationsErrors } from "./utils/index.js";
+
+mongoose.set("strictQuery", false);
+
+// mongodb+srv://admin:MMAfighter03@cluster0.x1zbe89.mongodb.net/blog?retryWrites=true&w=majority
+
+mongoose
+  .connect(
+    "mongodb://mongo:8uZKYJ4zV3vGGQGRSWeu@containers-us-west-141.railway.app:6079"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("err", err));
+
+const app = express();
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+app.post(
+  "/auth/login",
+  loginValidation,
+  handelValidationsErrors,
+  UserController.login
+);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handelValidationsErrors,
+  UserController.register
+);
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.get("/tags", PostController.getLastsTags);
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/tags", PostController.getLastsTags);
+app.get("/posts/:id", PostController.getOne);
+app.post(
+  "/posts",
+  checkAuth,
+  postCreateValidation,
+  handelValidationsErrors,
+  PostController.create
+);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handelValidationsErrors,
+  PostController.update
+);
+
+// 4444
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) throw err;
+
+  console.log("Server OK");
+});
---
+++ utils/checkAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+  const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
+
+  if (token) {
+    try {
+      const deoded = jwt.verify(token, "secret123");
+
+      req.userId = deoded._id;
+
+      next();
+    } catch (error) {
+      return res.status(403).json({
+        message: "Нет доступа",
+      });
+    }
+  } else {
+    return res.status(403).json({
+      message: "Нет доступа",
+    });
+  }
+};
